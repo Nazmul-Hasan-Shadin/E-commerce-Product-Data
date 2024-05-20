db1389a370399f5345926225a0c9ff05d98bf39d
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-05-20 22:47:57 +0600

    interface and product model has added

---
+++ src/models/products/product.model.ts
@@ -0,0 +1,26 @@
+import { Schema, model } from 'mongoose';
+import {  TInventory, TProducts, TVariants } from './product.interface';
+
+const variants= new Schema<TVariants>({
+    type:String,
+    value:String
+})
+
+ const inventory=new Schema<TInventory>({
+    quantity:{type:Number},
+    inStock:Boolean
+ })
+
+const productSchema=new Schema<TProducts>({
+    name:{type:String,required:true},
+    description:{type:String,required:true},
+    price:{type:Number,required:true},
+    category:{type:String,required:true},
+    tags:{type:[String],default:undefined},
+    variants:[variants],
+    inventory:inventory
+     
+
+})
+
+export const Product= model<TProducts>('Product',productSchema)
