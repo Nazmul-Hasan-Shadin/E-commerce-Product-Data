4e7a6b56fef2981f0d7522df5b3488d3b38481e8
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-05-21 00:27:35 +0600

    product query by id feature has added

--- src/models/products/product.controller.ts
+++ src/models/products/product.controller.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import { ProductServices } from "./product.services";
+import mongoose from "mongoose";
+
+// post product api
 
 const createProduct = async (req: Request, res: Response) => {
   try {
     const productData = req.body;
-    const result =await ProductServices.createProductIntoDb(productData);
+    const result = await ProductServices.createProductIntoDb(productData);
     res.json({
       success: true,
       message: "Product created successfully!",
@@ -18,6 +21,48 @@ const createProduct = async (req: Request, res: Response) => {
   }
 };
 
-export const ProductController={
-    createProduct
-}
+// find all product api
+
+const getAllProduct = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProductFromDb();
+    res.json({
+      success: true,
+      message: "Product fetched successfully!",
+      data: result,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "something went wrong",
+    });
+  }
+};
+
+// getSingle product by id
+
+const getSingleProduct = async (req: Request, res: Response) => {
+  try {
+    const stringId=req.params.productId
+    const paramId = new mongoose.Types.ObjectId(stringId)
+    
+    
+    const result =await ProductServices.getSingleProductFromDb(paramId);
+    res.json({
+      success: true,
+      message: "Product fetched successfully!",
+      data: result,
+    });
+  } catch (error:any) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "something went wrong",
+    });
+  }
+};
+
+export const ProductController = {
+  createProduct,
+  getAllProduct,
+  getSingleProduct
+};
--- src/models/products/product.services.ts
+++ src/models/products/product.services.ts
@@ -1,15 +1,30 @@
-
 import { Product } from "./product.model";
 import { TProducts } from "./product.interface";
+import mongoose from "mongoose";
 
-const createProductIntoDb=async(payload:TProducts)=>{
+const createProductIntoDb = async (payload: TProducts) => {
+  const result = await Product.create(payload);
+  return result;
+};
 
-    const result=await Product.create(payload)
-    return result
-}
+// get all product api
 
+const getAllProductFromDb = async () => {
+  const result = await Product.find();
+  return result;
+};
 
-export const ProductServices={
-    createProductIntoDb,
+// search product by id
 
-}
+const getSingleProductFromDb = async (payload:mongoose.Types.ObjectId) => {
+  const result = await Product.findById(payload);
+  console.log(result);
+  
+  return result;
+};
+
+export const ProductServices = {
+  createProductIntoDb,
+  getAllProductFromDb,
+  getSingleProductFromDb
+};
